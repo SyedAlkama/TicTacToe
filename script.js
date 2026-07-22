6fa30f85e7e6726eab08e06d5131f435a4a7004f
Syed Alkama <syedalkama2419@gmail.com>
2026-07-22 18:57:59 +0530

    Completed the game

--- script.js
+++ script.js
@@ -1,74 +1,117 @@
-let game = (function(){
+let game = (function(){  // Factory Function for the game object
     let gameboard = [1,2,3,4,5,6,7,8,9];
 
-    let players = ['X','O'];
     let scores = {
-        [players[0]]:0,
-        [players[1]]:0
+        'X':0,
+        'O':0
     };
 
-    let playerTurn = players[0];
+    let playerTurn = 'X'; // Which Player's Turn 
+    let roundMoves = 0; // How many moves in this round
 
+    // reset all variables to start the new round but keeps the score
+    function startNewRound(){
+        roundMoves = 0;
+        playerTurn = 'X';
+        gameboard = [1,2,3,4,5,6,7,8,9];
+    }
+
+    // If the player exists increase its score
     function gameWinBy(winner){
-        if(!players.includes(winner)){
-            throw new Error("This Player Doesn't exist");
-        }
         scores[winner]++;
     }
-    function resetGame(){
-        for(player of players){
-            scores[player] = 0;
-        }
+
+    // Reset the score to 0-0
+    function resetScore(){
+        scores['X']=0;
+        scores['O']=0;
     }
+
+    // play in one of the squares(1-9), changes the player itself 
     function playSquare(square){
-        gameboard[square] = playerTurn;
+        gameboard[square-1] = playerTurn;
         if (playerTurn == 'X'){
             playerTurn='O';
         } else {
             playerTurn='X';
         }
-        return gameboard;
+        roundMoves++;
     }
+
+    // Gets the score
     function getScore(){
         return scores;
     }
+
+    // Tells which player's turn it is
     function getPlayerTurn(){
         return playerTurn;
     }
-    return {gameWinBy,resetGame,playSquare,getScore,getPlayerTurn};
+
+    // Check if all the squares are filled
+    function roundComplete(){
+        return (roundMoves==9);
+    }
+
+    //Returns the Gameboard
+    function getGameboard(){
+        return gameboard;
+    }
+
+    return {gameWinBy,resetScore,playSquare,getScore,getPlayerTurn,roundComplete,getGameboard,startNewRound};
 })();
-let gameOn = false;
-while (gameOn){
-    gameON()
-    gameOn = false;
-}
-function gameON(){
-    let gameTurn = 0;
-    while(gameTurn != 9){
 
-        let playing = game.getPlayerTurn();
+let squares = document.getElementsByName("squares");
+let feedback = document.getElementById("feedback");
 
-        console.log(`${playing} Turn!`);
 
-        let chance = prompt("pick a square between 1-9:");
-        let gameboard = game.playSquare(chance-1);
-        console.log(gameboard);
+// Reset Everything and start a New round 
+document.getElementById("resetBtn").addEventListener("click",()=>{
+    resetGrid();
+    game.resetScore();
+    updateScore();
+    game.startNewRound();
+    feedback.textContent= `${game.getPlayerTurn()} Turn!`;
+})
 
-        if (winCondition(gameboard)){
-            game.gameWinBy(playing);
-            gameTurn = 0;
-            console.log(`${playing} WONNNN!!!`);
-            break;
-        }
-        gameTurn++;
+Array.from(squares).forEach((square)=>{
+    square.addEventListener('click',()=>{
+        // Get which player turn , fill the square clicked with that player
+        let player = game.getPlayerTurn();
+        square.textContent = player;
+        // play on that sqaure and disable the button 
+        game.playSquare(square.id);
+        square.disabled = true;
+        // Check for a win or tie
+        checkIfGameEnd(player);
+    })
+})
 
+function checkIfGameEnd(player){
+    if (winCondition(game.getGameboard())){
+        game.gameWinBy(player);
+        feedback.textContent= `${player} WONNNN!!!`;
+        game.startNewRound();
+        resetGrid();
+        updateScore();
+    }else if(game.roundComplete()){
+        feedback.textContent = "Its a TIEEEEEE";
+        game.startNewRound();
+        resetGrid();
+    }else{
+        feedback.textContent= `${game.getPlayerTurn()} Turn!`;
     }
-    if (gameTurn==9){
-        console.log("TIEEEEEEE!!!")
-    }
-    console.log(game.getScore());
 }
 
+// reset the grid to blank 
+function resetGrid(){
+    squares.forEach((square)=>{
+        square.textContent = "";
+        square.disabled = false;
+    })
+}
+
+// All winning conditions 
 function winCondition(board){
     return ((board[0]==board[1] && board[1]==board[2])
             || (board[2]==board[5] && board[5]==board[8])
@@ -78,4 +121,11 @@ function winCondition(board){
             || (board[2]==board[4] && board[4]==board[6])
             || (board[3]==board[4] && board[4]==board[5])
             || (board[1]==board[4] && board[4]==board[7]));
+}
+
+// Update the Score counter
+function updateScore(){
+    let scoreCounter = document.getElementById("scoreCounter");
+    let score = game.getScore();
+    scoreCounter.textContent = `X: ${score['X']} | O : ${score['O']}`;
 }
